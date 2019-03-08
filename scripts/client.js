c1c34a3ea1feca4ff8233722fca89fbd4bfbf4c3
Rachel Peddie <rachel.peddie.915@gmail.com>
2019-03-08 17:17:11 -0600

    created empty array to push employees into, created class constructor for adding a new employee, created function to override default clear on form inputs and run function addEmployee on click, set up variables for addEmployee function

--- scripts/client.js
+++ scripts/client.js
@@ -2,7 +2,39 @@ console.log('js');
 
 $(document).ready(readyNow);
 
+// declare empty array to input new employee vals
+let employeeInfo = [];
+// declare variable to hold monthly cost
+// let costMonthly = 
+
+// create a constructor to create new object for each employee
+class Employee {
+    constructor( firstName, lastName, id, title, annualSalary ){
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.id = id,
+    this.title = title,
+    this.annualSalary = annualSalary
+    }
+} // end Employee class
+
+function addEmployee() {
+    let employeeFirstName = $('#firstNameInput').val();
+    let employeeLastName = $('#lastNameInput').val();
+    let employeeId = $('#idInput').val();
+    let employeeTitle = $('#titleInput').val();
+    let employeeAnnualSalary = $('#annualSalaryInput').val();
+
+}
+
+function buttonSubmit(event) {
+    console.log('Employee info has been added!');
+    event.preventDefault();
+    addEmployee();
+}
+
+
 function readyNow() {
     console.log('jq');
-    
-}
+    $('#addToFormButton').on('click', buttonSubmit);
+} // end readyNow
