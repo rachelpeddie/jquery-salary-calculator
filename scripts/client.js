3e9a1d7646020a213988843f5f404ea3e19479d9
Rachel Peddie <rachel.peddie.915@gmail.com>
2019-03-08 18:03:01 -0600

    created new employee using class and previously declared variables that pushes new employee to employeeInfo array, calls addEmployeeToTable function which needs to be written

--- scripts/client.js
+++ scripts/client.js
@@ -5,7 +5,12 @@ $(document).ready(readyNow);
 // declare empty array to input new employee vals
 let employeeInfo = [];
 // declare variable to hold monthly cost
-// let costMonthly = 
+// let costMonthly =
+
+function readyNow() {
+    console.log('jq');
+    $('#addToFormButton').on('click', buttonSubmit);
+} // end readyNow
 
 // create a constructor to create new object for each employee
 class Employee {
@@ -18,23 +23,27 @@ class Employee {
     }
 } // end Employee class
 
+// function to add new employee with properties to array from inputs
 function addEmployee() {
+    // declared variables equal to input values for legibility
     let employeeFirstName = $('#firstNameInput').val();
     let employeeLastName = $('#lastNameInput').val();
     let employeeId = $('#idInput').val();
     let employeeTitle = $('#titleInput').val();
     let employeeAnnualSalary = $('#annualSalaryInput').val();
+    
+    //create a new employee object with variables
+    let newEmployee = new Employee ( employeeFirstName, employeeLastName, employeeId, employeeTitle, employeeAnnualSalary );
 
+    // push new object to array and log for confirmation
+    employeeInfo.push(newEmployee);
+    console.log('New Employee: ', employeeInfo);
+    addEmployeeToTable();
 }
+
 
 function buttonSubmit(event) {
     console.log('Employee info has been added!');
     event.preventDefault();
     addEmployee();
 }
-
-
-function readyNow() {
-    console.log('jq');
-    $('#addToFormButton').on('click', buttonSubmit);
-} // end readyNow
